900ebe48092816bdab13947773d9447b77718bbb
Hana Volenik <hana.volenik6@gmail.com>
2023-04-14 20:41:18 +0200

    Final changes and formatting documents

--- members.js
+++ members.js
@@ -1,68 +1,67 @@
-import {drone} from "/script.js"
+import { drone } from "/script.js";
 
 const DOM = {
-    membersCount: document.querySelector('.members-count'),
-    membersList: document.querySelector('.members-list'),
-    messages: document.querySelector('.messages'),
-    input: document.querySelector('.message-form__input'),
-    form: document.querySelector('.message-form'),
-  };
-  
-  DOM.form.addEventListener('submit', sendMessage);
-  
-  function sendMessage() {
-    const value = DOM.input.value;
-    if (value.trim().length === 0) {
-      return;
-    }
-    DOM.input.value = '';
-    drone.publish({
-      room: 'observable-room',
-      message: value,
-    });
-  }
+  membersCount: document.querySelector(".members-count"),
+  membersList: document.querySelector(".members-list"),
+  messages: document.querySelector(".messages"),
+  input: document.querySelector(".message-form__input"),
+  form: document.querySelector(".message-form"),
+};
 
-  function createMemberElement(member) {
-    const { name, color } = member.clientData;
-    const el = document.createElement('div');
-    if (drone.clientId === member.id) {
-      el.appendChild(document.createTextNode(name + ' * '));
-    } else {
-      el.appendChild(document.createTextNode(name));
-    }
-    el.className = 'member';
-    el.style.color = color;
-    return el;
+DOM.form.addEventListener("submit", sendMessage);
+
+function sendMessage() {
+  const value = DOM.input.value;
+  if (value.trim().length === 0) {
+    return;
   }
-  
-  export function updateMembersDOM(members) {
-    if (members.length > 1) {
-      DOM.membersCount.innerText = `${members.length} users in the mood to chat:`;
-    }
-    else {
-      DOM.membersCount.innerText = `${members.length} user in the mood to chat:`;
-    }
-    DOM.membersList.innerHTML = '';
-    members.forEach(member =>
-      DOM.membersList.appendChild(createMemberElement(member))
-    );
+  DOM.input.value = "";
+  drone.publish({
+    room: "observable-room",
+    message: value,
+  });
+}
+
+function createMemberElement(member) {
+  const { name, color } = member.clientData;
+  const el = document.createElement("div");
+  if (drone.clientId === member.id) {
+    el.appendChild(document.createTextNode(name + " * "));
+  } else {
+    el.appendChild(document.createTextNode(name));
   }
-  
-  function createMessageElement(text, member) {
-  const el = document.createElement('div');
+  el.className = "member";
+  el.style.color = color;
+  return el;
+}
+
+export function updateMembersDOM(members) {
+  if (members.length > 1) {
+    DOM.membersCount.innerText = `${members.length} users in the mood to chat:`;
+  } else {
+    DOM.membersCount.innerText = `${members.length} user in the mood to chat:`;
+  }
+  DOM.membersList.innerHTML = "";
+  members.forEach((member) =>
+    DOM.membersList.appendChild(createMemberElement(member))
+  );
+}
+
+function createMessageElement(text, member) {
+  const el = document.createElement("div");
   el.appendChild(createMemberElement(member));
-  el.appendChild(document.createElement('br'));
+  el.appendChild(document.createElement("br"));
   el.appendChild(document.createTextNode(text));
   if (drone.clientId === member.id) {
-    el.className = 'message outgoing';
+    el.className = "message outgoing";
   } else {
-    el.className = 'message incoming';
+    el.className = "message incoming";
   }
   return el;
 }
-  
-  export function addMessageToListDOM(text, member) {
-    const el = DOM.messages;
-    el.appendChild(createMessageElement(text, member));
-    el.scrollTop = el.scrollHeight - el.clientHeight;
-  }
+
+export function addMessageToListDOM(text, member) {
+  const el = DOM.messages;
+  el.appendChild(createMessageElement(text, member));
+  el.scrollTop = el.scrollHeight - el.clientHeight;
+}
